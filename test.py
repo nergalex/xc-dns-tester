1e1e2147effb3b88b4d2c1b00cabd27293d62971
nergalex <nergalex@hotmail.com>
2025-03-13 09:31:51 +0100

    while

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import unittest
 from scapy.all import *
 from scapy.layers import inet
 from scapy.layers.dns import (
@@ -10,30 +9,31 @@ from scapy.layers.dns import (
 def test_dns():
     dst_ip = "8.8.8.8"
     dst_port = 53
-    fqdn = "monip.io"
+    fqdn = "test.me"
     record_type = "A"
 
-    dns_request = inet.IP(dst=dst_ip) \
-              / fuzz(inet.UDP(sport=59890, dport=dst_port)) \
-              / fuzz(DNS(qr=0,
-                         opcode=0,
-                         aa=0,
-                         tc=0,
-                         rd=1,
-                         z=0,
-                         ad=0,
-                         cd=0,
-                         rcode=0,
-                         qd=DNSQR(
-                             qname=fqdn,
-                             qtype=record_type,
-                             unicastresponse=0,
-                             qclass='IN'
-                         )))
+    while True:
+        dns_request = inet.IP(dst=dst_ip) \
+                  / fuzz(inet.UDP(sport=59890, dport=dst_port)) \
+                  / fuzz(DNS(qr=0,
+                             opcode=0,
+                             aa=0,
+                             tc=0,
+                             rd=1,
+                             z=0,
+                             ad=0,
+                             cd=0,
+                             rcode=0,
+                             qd=DNSQR(
+                                 qname=fqdn,
+                                 qtype=record_type,
+                                 unicastresponse=0,
+                                 qclass='IN'
+                             )))
 
-    answer = sr1(dns_request, verbose=1)
-
-    print(answer[DNS].summary())
+        answer = sr1(dns_request, verbose=1)
+        print(answer[DNS].summary())
+        # time.sleep(0.01)
 
 
 if __name__ == '__main__':
